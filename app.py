87830744c8af43ac159d25fa833c7b1ceccdcf25
Miriã F. Costa <72331678+mifiaas@users.noreply.github.com>
2025-11-01 02:23:52 -0300

    Fix Google Sheets URL and clean up app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ REDCAP_API_URL = "https://redcap.fcmsantacasasp.edu.br/api/"
 REDCAP_TOKEN = "2B90EF2F5C5A59B08A6655751F613365"
 
 # URL do Google Apps Script
-GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbxXrl83NGgJxwhuUVVF6f8OZU5bqFdPX45ElTAAIAOsE0eWg0PkzART-oH-L6VacjL-/exec"
+GOOGLE_SHEETS_URL = "https://script.google.com/macros/s/AKfycbzRx14Wtnfh_zWq9nt-MKEXwcrhMGgRVRAHiue-dC8vr6fsZHdmYhp3G7BDKqGnTJ94/exec"
 
 @app.route("/")
 def home():
@@ -63,6 +63,7 @@ def redcap():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
 
 
 
